7217f0d30f30777b35da20c7c912b1a305cfb1e6
Isabel <isabel-gaubatz@t-online.de>
2018-02-12 15:36:46 +0100

    HoloToolKit import und Testen des KeywordManagers in der Main Scene an einem Cube

---
+++ Assets/ColorChange.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChange : MonoBehaviour {
+	TextMesh textMesh;
+	// Use this for initialization
+	void Start () {
+		textMesh = GetComponent<TextMesh> ();
+	}
+	
+	// Update is called once per frame
+	public void blue () {
+		textMesh.color = Color.blue;
+	}
+
+	public void red () {
+		textMesh.color = Color.red;
+	}
+
+	public void green () {
+		textMesh.color = Color.green;
+	}
+}
