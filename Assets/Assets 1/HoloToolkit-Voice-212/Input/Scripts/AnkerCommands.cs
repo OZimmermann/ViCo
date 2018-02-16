31b183dda31db291bd6e8389a638bcd85347a73a
Romina Marsico <rominamarsico@users.noreply.github.com>
2018-02-16 19:02:12 +0100

    Menu Level 2 erscheint nicht, nächster Versuch: Übergabe aus anderem Script

--- Assets/Assets 1/HoloToolkit-Voice-212/Input/Scripts/AnkerCommands.cs
+++ Assets/Assets 1/HoloToolkit-Voice-212/Input/Scripts/AnkerCommands.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class AnkerCommands : MonoBehaviour
 {
+
+    public MenuCommands script = GameObject.Find("Erinnerung").GetComponent<MenuCommands>();
+
     // Menu hirachy level 1
     public GameObject Pokal;
     public GameObject Hantel;
@@ -12,40 +15,10 @@ public class AnkerCommands : MonoBehaviour
     public GameObject Erinnerung;
     public GameObject SurvivalKit;
 
-    // Other menu items
-    /*public GameObject Draußen;
-    public GameObject Drinnen;
-    public GameObject Einkaufen;
-    public GameObject Telefon;*/
-    public GameObject Graph;
-    public GameObject Pyramide;
-
-    void Awake()
-    {
-        // Menu items on second and further hirachy level are disabled
-        Erinnerung = GameObject.FindWithTag("Erinnerung");
-        Erinnerung.SetActive(false);
-        SurvivalKit = GameObject.FindWithTag("SurvivalKit");
-        SurvivalKit.SetActive(false);
-
-        // Further menu items are disabled
-        /*  Draußen = GameObject.FindWithTag("Draußen");
-          Draußen.SetActive(false);
-          Drinnen = GameObject.FindWithTag("Drinnen");
-          Drinnen.SetActive(false);
-          Einkaufen = GameObject.FindWithTag("Einkaufen");
-          Einkaufen.SetActive(false);
-          Telefon = GameObject.FindWithTag("Telefon");
-          Telefon.SetActive(false);*/
-        Graph = GameObject.FindWithTag("Graph");
-        Graph.SetActive(false);
-        Pyramide = GameObject.FindWithTag("Pyramide");
-        Pyramide.SetActive(false);
-    }
-
     // Called by GazeGestureManager when the user performs a Select gesture
     void OnSelect()
     {
+
         // Menu disappears when the user selects one option
         gameObject.SetActive(false); // gameObject: Anker
         Pokal = GameObject.FindWithTag("Pokal");
@@ -54,7 +27,9 @@ public class AnkerCommands : MonoBehaviour
         Hantel.SetActive(false);
 
         // Next menu items appear
+        Erinnerung = GameObject.FindWithTag("Erinnerung");
         Erinnerung.SetActive(true);
+        SurvivalKit = GameObject.FindWithTag("SurvivalKit");
         SurvivalKit.SetActive(true);
     }
 }
---
+++ Assets/Assets 1/HoloToolkit-Voice-212/Input/Scripts/MenuCommands.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuCommands : MonoBehaviour
+{
+    public GameObject Erinnerung;
+    public static GameObject SurvivalKit;
+    public static GameObject Pyramide;
+    public static GameObject Graph;
+
+    // Use this for initialization
+    void Awake()
+    {
+        Erinnerung = GameObject.FindWithTag("Erinnerung");
+        Erinnerung.SetActive(false);
+        SurvivalKit = GameObject.FindWithTag("SurvivalKit");
+        SurvivalKit.SetActive(false);
+        Graph = GameObject.FindWithTag("Graph");
+        Graph.SetActive(false);
+        Pyramide = GameObject.FindWithTag("Pyramide");
+        Pyramide.SetActive(false);
+    }
+}
