7fd61caed8652386efd4c9a040c40d3d0fdc0ec8
Olga Zimmermann <o.zimmermann@meso.net>
2018-02-19 14:22:42 +0100

    first voice commands for introduction added

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Academy.HoloToolkit.Unity;
+
+public class SoundManager : MonoBehaviour {
+
+	public AudioSource introduction;
+	public KeywordManager keywordManager;
+
+	void Start () {
+		introduction = GetComponent<AudioSource>();
+		keywordManager = GetComponent<KeywordManager> ();
+
+		introduction.Play ();
+		Debug.Log ("Ich spiele introduction ab");
+		//source.Play(44100);
+	}
+
+	void Update () {
+		if (!introduction.isPlaying) {
+			keywordManager.StartKeywordRecognizer ();
+			//Debug.Log ("KeywordManager started");
+		}
+	}
+
+	public void Gender () {
+		Debug.Log ("männlich");
+	}
+}
