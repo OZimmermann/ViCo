34bcb25f2461251a56fc0029260f9d82e71696c4
Olga Zimmermann <o.zimmermann@meso.net>
2018-02-19 15:45:50 +0100

    added voice commands and sounds

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -2,29 +2,80 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Academy.HoloToolkit.Unity;
+using UnityEngine.SceneManagement;
 
 public class SoundManager : MonoBehaviour {
 
-	public AudioSource introduction;
+	public AudioSource start;
+	public AudioSource gender;
+	public AudioSource owl;
+	public AudioSource end;
 	public KeywordManager keywordManager;
 
+	public int stage = 1;
+	/*
+	 * stage = 1 // introduction
+	 * stage = 2 // ChooseGender
+	 * stage = 3 // ChooseOwl
+	 */
+
 	void Start () {
-		introduction = GetComponent<AudioSource>();
 		keywordManager = GetComponent<KeywordManager> ();
 
-		introduction.Play ();
+		start.Play ();
 		Debug.Log ("Ich spiele introduction ab");
 		//source.Play(44100);
 	}
 
 	void Update () {
-		if (!introduction.isPlaying) {
-			keywordManager.StartKeywordRecognizer ();
-			//Debug.Log ("KeywordManager started");
+		switch (stage) {
+		case 1:
+			if (!start.isPlaying) {
+				keywordManager.StartKeywordRecognizer ();
+				//Debug.Log ("KeywordManager started");
+			}
+			break;
+		case 2: 
+			if (!gender.isPlaying) {
+				keywordManager.StartKeywordRecognizer ();
+				//Debug.Log ("KeywordManager started");
+			}
+			break;
+		case 3: 
+			if (!owl.isPlaying) {
+				keywordManager.StartKeywordRecognizer ();
+				//Debug.Log ("KeywordManager started");
+			}
+			break;
+		case 4: 
+			if (!end.isPlaying) {
+				SceneManager.LoadScene ("therapeuten_uebung", LoadSceneMode.Additive);
+			}
+			break;
+		default:
+			break;
 		}
+
 	}
 
-	public void Gender () {
-		Debug.Log ("männlich");
+	public void ChooseGender () {
+		stage++;
+		keywordManager.StopKeywordRecognizer ();
+		gender.Play ();
+		Debug.Log ("Sag: männlich");
+	}
+
+	public void ChooseOwl () {
+		stage++;
+		keywordManager.StopKeywordRecognizer ();
+		owl.Play ();
+		Debug.Log ("Sag: Sag Waldkauz/Uhu/Schneeeule");
+	}
+
+	public void EndIntroduction () {
+		stage++;
+		keywordManager.StopKeywordRecognizer ();
+		Debug.Log ("Initialisierung beendet");
+		end.Play ();
 	}
 }
