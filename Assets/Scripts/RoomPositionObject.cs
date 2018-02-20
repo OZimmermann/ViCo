c44cae6f5e81cea0a7f55bbb44441e5bf3fe56e1
Isabel <isabel-gaubatz@t-online.de>
2018-02-20 13:21:37 +0100

    Add RoomPositionObject Script
    Test ob es ohne Air Tab geht, die Pyramide auf dem Boden zu plazieren.

---
+++ Assets/Scripts/RoomPositionObject.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class RoomPositionObject : MonoBehaviour
+{
+
+	// Update is called once per frame
+	void Update()
+	{
+			// Do a raycast into the world that will only hit the Spatial Mapping mesh.
+			var headPosition = Camera.main.transform.position;
+			var gazeDirection = Camera.main.transform.forward;
+
+			RaycastHit hitInfo;
+			if (Physics.Raycast(headPosition, gazeDirection, out hitInfo,
+				30.0f, SpatialMapping.PhysicsRaycastMask))
+			{
+				// Move this object's parent object to
+				// where the raycast hit the Spatial Mapping mesh.
+				this.transform.parent.position = hitInfo.point;
+
+				// Rotate this object's parent object to face the user.
+				Quaternion toQuat = Camera.main.transform.localRotation;
+				toQuat.x = 0;
+				toQuat.z = 0;
+				this.transform.parent.rotation = toQuat;
+			}
+
+	}
+}
